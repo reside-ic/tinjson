94437a4444d4d362d5cfb498687f803b2b4bec47
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-09-06 12:33:22 +0100

    Add additional test

--- tests/testthat/test-format.R
+++ tests/testthat/test-format.R
@@ -10,6 +10,7 @@ test_that("can roundtrip simple json", {
   expect_can_roundtrip("false")
   expect_can_roundtrip("null")
   expect_can_roundtrip("1")
+  expect_can_roundtrip("[1]")
   expect_can_roundtrip('"something"')
   expect_can_roundtrip("[1,2,3]")
   expect_can_roundtrip('{"a":1,"b":2}')
